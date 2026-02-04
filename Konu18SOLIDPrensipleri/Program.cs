46955a151dbb07b71fb14d1cd55adac15ab23177
Mesut ILICA <mesut.ilica@gmail.com>
2026-02-04 22:30:18 +0300

    4-2-26 win forms

---
+++ Konu18SOLIDPrensipleri/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Konu18SOLIDPrensipleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SOLID Prensipleri");
+            Console.WriteLine("Robert Cecil Martin tarafından öne sürülen geliştirilecek yazılımın esnek, yeniden kullanılabilir, sürdürülebilir ve anlaşılır olmasını hedefleyen, kod tekrarını önleyen prensipler bütünüdür.");
+            Console.WriteLine();
+
+            Console.WriteLine("S - Single Responsibility(Tek Sorumluluk Prensibi)");
+            Console.WriteLine(" \t 1 sınıf veya metodun sadece 1 sorumluluğunun olması gerekir!");
+
+            Console.WriteLine();
+
+            Console.WriteLine("O - Open-Closed Principle");
+            Console.WriteLine("\t Bir sınıf veya metot var olan özelliklerini korumalı değişikliğe izin vermemelidir");
+
+            Console.WriteLine();
+
+            Console.WriteLine("L - Liskov Substitution");
+            Console.WriteLine("Kodlarımızda her hangi bir değişiklik yapmaya gerek duymadan alt sınıfları türedikleri üst sınıfların yerine kullanabilmeliyiz.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("I - Interface Segregation");
+            Console.WriteLine("Sorumlulukların hepsini tek bir arayüzde toplamak yerine özelleştirilmiş birden fazla arayüz kullanmalıyız.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("D - Dependency Inversion");
+            Console.WriteLine("Sınıflar arası bağımlılıklar olabildiğince az olmalıdır. Özellikle üst seviye sınıflar alt seviye sınıflara bağımlı olmamalıdır.");
+        }
+    }
+}
