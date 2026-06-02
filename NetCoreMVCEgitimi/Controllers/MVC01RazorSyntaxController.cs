bbff4df72ec0c06ede270dd2987cc4252c534c1c
Mesut ILICA <mesut.ilica@gmail.com>
2026-06-02 22:03:15 +0300

    2-6-26 Mvc 02

---
+++ NetCoreMVCEgitimi/Controllers/MVC01RazorSyntaxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMVCEgitimi.Controllers
+{
+    public class MVC01RazorSyntaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCoreMVCEgitimi/Controllers/MVC02HtmlHelpersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMVCEgitimi.Controllers
+{
+    public class MVC02HtmlHelpersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
