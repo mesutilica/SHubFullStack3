e713c9302c71a3b5a16abcb3bc5c49a247f8a342
Mesut ILICA <mesut.ilica@gmail.com>
2026-06-16 22:59:12 +0300

    16-06-26 Mvc kuki

---
+++ NetCoreMVCEgitimi/Controllers/MVC10FileUploadController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMVCEgitimi.Controllers
+{
+    public class MVC10FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(IFormFile? dosya)// Mvc de dosya yükleme IFormFile interface i ile yapılıyor. Burada dosya isminin ekrandaki file upload name i ile aynı olması gerekir yoksa dosya yüklenmez!
+        {
+            if (dosya != null)
+            {
+                var uzanti = Path.GetExtension(dosya.FileName);
+                var klasor = Directory.GetCurrentDirectory() + "/wwwroot/Images/"; // resmin yükleneceği klasör
+                var klasorVarmi = Directory.Exists(klasor); // sunucuda bu klasör var mı?
+                TempData["Message"] = "klasorVarmi : " + klasorVarmi;
+                if (klasorVarmi == false) // eğer sunucuda bu konumda klasör yoksa
+                {
+                    var sonuc = Directory.CreateDirectory(klasor); // ana dizine Images klasörü oluştur
+                    TempData["Message"] += " - Klasör Oluşturuldu.. " + sonuc;
+                }
+                if (uzanti == ".jpg" || uzanti == ".jpeg" || uzanti == ".png" || uzanti == ".gif") // Sadece bu uzantılardaki dosyaları kabul et
+                {
+                    // 1. Yöntem Random(Rastgele) İsimle Dosya Yükleme
+                    /*
+                    var randomFileName = Path.GetRandomFileName(); // rasgele dosya ismi oluşturma metodu
+                    var fileName = Path.ChangeExtension(randomFileName, ".jpg"); // dosya adı ve uzantısını değiştirip birleştirdik
+                    var path = Path.Combine(klasor, fileName); // klasör ve resim adını birleştirdik
+
+                    using var stream = new FileStream(path, FileMode.Create); // resmi farklı kaydet metoduyla sunucuya yüklüyoruz.
+
+                    dosya.CopyTo(stream); // resmi sunucuya yükle
+                    TempData["Resim"] = fileName;
+                    */
+                    // 2. Yöntem - Resmi Kendi Adıyla Yükleme
+                    /*
+                    var dosyaAdi = Path.GetFileName(dosya.FileName);
+                    var yol = Path.Combine(klasor, dosyaAdi);
+                    using var stream = new FileStream(yol, FileMode.Create);
+                    dosya.CopyTo(stream); // resmi sunucuya yükle
+                    TempData["Resim"] = dosyaAdi; // yüklenen dosya adı
+                    */
+                    // 3. yöntem, resmi direk sunucuya yollama
+                    using var stream = new FileStream(klasor + dosya.FileName, FileMode.Create);
+                    dosya.CopyTo(stream);
+                    TempData["Resim"] = dosya.FileName;
+                }
+                else
+                {
+                    TempData["Message"] += " - Sadece .jpg, .jpeg, .png, .gif uzantılı dosyalar yüklenebilir!";
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ResimSil(string resimYolu)
+        {
+            if (System.IO.File.Exists(resimYolu))
+            {
+                System.IO.File.Delete(resimYolu);
+                TempData["Message"] = "Resim Silindi!";
+                return RedirectToAction("Index");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ NetCoreMVCEgitimi/Controllers/MVC11CookieController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCEgitimi.Models;
+
+namespace NetCoreMVCEgitimi.Controllers
+{
+    public class MVC11CookieController : Controller
+    {
+        UyeContext context = new UyeContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CookieOlustur(string kullaniciAdi, string sifre)
+        {
+            var kullanici = context.Uyeler.FirstOrDefault(u => u.KullaniciAdi == kullaniciAdi && u.Sifre == sifre);
+            if (kullanici != null)
+            {
+                Response.Cookies.Append("userguid", Guid.NewGuid().ToString());
+                var cookieAyarlari = new CookieOptions()
+                {
+                    Expires = DateTime.Now.AddMinutes(1) // cookie ye 1 dk lık bitiş süresi tanımladık
+                };
+                HttpContext.Response.Cookies.Append("username", kullaniciAdi, cookieAyarlari); // .net de HttpContext ile oluşturuyoruz
+                return RedirectToAction("CookieOku");
+            }
+            else
+            {
+                TempData["Mesaj"] = @"<div class='alert alert-danger'>Giriş Başarısız!</div>";
+            }
+            return View();
+        }
+    }
+}
